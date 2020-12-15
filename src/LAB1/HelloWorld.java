5d100dd769e7298d50a4beeb556e9fc546fecc70
vachakorn <75651222+vachakorn@users.noreply.github.com>
2020-12-15 08:50:25 +0700

    Update HelloWorld.java

--- src/LAB1/HelloWorld.java
+++ src/LAB1/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Helloworld");
+        System.out.println("Helloworld");
+        System.out.println("Helloworld");
+        System.out.println("Helloworld");
 
 
     } //main

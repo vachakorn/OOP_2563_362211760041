175502ac34cdf2a8f512cd18f0764ed599b0ed49
vachakorn <vachakorn_palm123@rmutsvmail.com>
2020-12-29 11:42:22 +0700

    LAB2

---
+++ src/LAB2/Exercise_STD_Profile.java
@@ -0,0 +1,30 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class Exercise_STD_Profile {
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        System.out.print("Name:");
+        String a = name.nextLine();
+
+        Scanner id = new Scanner(System.in);
+        System.out.print("id:");
+        String b = name.nextLine();
+
+        Scanner age = new Scanner(System.in);
+        System.out.print("age:");
+        String c = name.nextLine();
+
+        Scanner tel = new Scanner(System.in);
+        System.out.print("tel:");
+        String d = name.nextLine();
+
+        Scanner email = new Scanner(System.in);
+        System.out.print("email:");
+        String e = name.nextLine();
+
+
+    }
+
+}
